5b63f79bfc436a0fbf47ec3a7d5dc368dd9930b7
unknown <falconfinance.alex@gmail.com>
2022-08-25 13:42:11 +0300

    Day/Night switcher added

--- markup/js/main.js
+++ markup/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
-  
+  // Day/Night switcher
+    $('.switch').click(function(){
+      $(this).toggleClass('night');
+      $(this).toggleClass('day');
+      if($(this).hasClass('day')){
+        $('.switch__circle::before').show(300);
+        $('.switch__circle::after').hide(300);
+        console.log('day');
+      }
+      if ($(this).hasClass('night')){
+        $('.switch__circle::before').hide(300);
+        $('.switch__circle::after').show(300);
+        console.log('night');
+      }
+    });
   });
